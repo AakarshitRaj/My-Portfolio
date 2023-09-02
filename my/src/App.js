50c8397c0a754b8c64c8989cb3176d103385f0ad
Aakarshit <rajaakarshit7@gmail.com>
2023-09-03 01:26:51 +0530

    second commit

--- my/src/App.js
+++ my/src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
 import Header from './component/Header';
+import Main from './component/Main';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+        <Header />
+        <Main />
+      
     </div>
   );
 }
---
+++ my/src/component/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Main.css'
+function Main() {
+  return (
+    <div className='Main'>Hello, my name is [Your Name]. I'm a [Your Profession or Career Stage] with a passion for [Your Area of Expertise or Interest]. Over the past [Number of Years] years, I've had the opportunity to work on various projects and gain experience in [Relevant Skills or Industries].
+
+    I hold a degree in [Your Degree] from [Your University] and have since been dedicated to [Briefly Mention Your Career Goals or What Drives You]. My most recent role at [Your Previous Company] allowed me to [Highlight a Key Achievement or Responsibility].
+    
+    What sets me apart is my [Unique Quality, Strength, or Skill] and my commitment to [Key Value or Work Ethic]. I thrive in [Describe Work Environments or Conditions You Excel In], and I'm always eager to take on new challenges and opportunities for growth.
+    
+    Outside of work, I enjoy [Hobbies or Interests] and [Anything Personal That You'd Like to Share if Appropriate]. I'm excited about the possibility of contributing my skills and expertise to [Company Name], and I look forward to discussing how I can add value to your team."
+    
+    Remember to tailor your introduction to the specific context and audience you're addressing. You can also adapt it to make it shorter or longer as needed.
+
+    </div>
+  )
+}
+
+export default Main
